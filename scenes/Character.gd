40b156d0309f77448a689f91d21f378322f60663
lfigueroacontreras <lucafiguec@gmail.com>
2021-06-06 21:22:42 -0400

    dash and walljump

--- scenes/Character.gd
+++ scenes/Character.gd
@@ -6,6 +6,9 @@ export var friction = 0.25
 export var air_friction = 0.02
 export var gravity = 250
 export var jump_force = 150
+var jump_number = 0
+var jumpWall = 150
+var wallJump = 60
 
 var motion = Vector2.ZERO
 
@@ -28,9 +31,29 @@ func _physics_process(delta):
 	
 	motion.y += gravity * delta
 	
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+	if is_on_floor() or nextToWall():
+		
+		jump_number = 2
+		
+		if Input.is_action_just_pressed("ui_up") and jump_number > 0:
 			motion.y = -jump_force
+			jump_number -= 1
+			if not is_on_floor() and nextToRightWall():
+				motion.x -= wallJump
+				motion.y -= jumpWall
+			if not is_on_floor() and nextToLeftWall():
+				motion.x += wallJump
+				motion.y -= jumpWall
+				
+		if nextToWall() and motion.y > 30:
+			motion.y = 30
+			if nextToRightWall():
+				sprite.flip_h = true
+				sprite.play("slideWall")
+			elif nextToLeftWall():
+				sprite.flip_h = false
+				sprite.play("slideWall")
+				
 		if !x_input:
 			motion.x = lerp(motion.x, 0, friction)
 	else:
@@ -38,4 +61,24 @@ func _physics_process(delta):
 			motion.y = -jump_force / 2
 		if !x_input:
 			motion.x = lerp(motion.x, 0, air_friction)
+			
+	if Input.is_action_just_pressed("ui_accept"):
+		dash()
+		
 	motion = move_and_slide(motion, Vector2.UP)
+
+func nextToWall():
+	return nextToRightWall() or nextToLeftWall()
+	
+func nextToRightWall():
+	return $RightWall.is_colliding()
+	
+func nextToLeftWall():
+	return $LeftWall.is_colliding()
+	
+func dash():
+	max_speed = 1400
+	$Timer.start()
+
+func _on_Timer_timeout():
+	max_speed = 90
