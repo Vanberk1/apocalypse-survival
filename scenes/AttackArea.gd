77e30114c02de3ee7fed8a0e02e05d02cbbcb449
Francisco Esquibel <f.esquibelalvarez@gmail.com>
2021-06-08 10:50:46 -0400

    Adding character attack

---
+++ scenes/AttackArea.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var dir = Vector2.RIGHT
+
+onready var sprite = $AttackAnimation
+
+func _ready():
+	if dir == Vector2.LEFT:
+		sprite.flip_h = true
+	sprite.play()
+
+func _on_AttackAnimation_animation_finished():
+	queue_free()
+
+func _on_AttackArea_body_entered(body):
+	print(body.name)
+	if body.is_in_group("character"):
+		body.queue_free()
--- scenes/Character.gd
+++ scenes/Character.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const AttackArea = preload("res://scenes/AttackArea.tscn")
+
 export var acceleration = 900
 export var max_speed = 90
 export var friction = 0.25
@@ -10,17 +12,23 @@ var jump_number = 0
 var jumpWall = 150
 var wallJump = 60
 
+var can_attack = true
+
+var direction = Vector2.RIGHT
 var motion = Vector2.ZERO
+var x_input
 
 onready var sprite = $AnimatedSprite
 
 func _physics_process(delta):
-	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	x_input = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
 	if x_input < 0:
 		sprite.flip_h = true
+		direction = Vector2.LEFT
 	elif x_input > 0:
 		sprite.flip_h = false
+		direction = Vector2.RIGHT
 	
 	if x_input:
 		motion.x += x_input * acceleration * delta
@@ -32,10 +40,8 @@ func _physics_process(delta):
 	motion.y += gravity * delta
 	
 	if is_on_floor() or nextToWall():
-		
 		jump_number = 2
-		
-		if Input.is_action_just_pressed("ui_up") and jump_number > 0:
+		if Input.is_action_just_pressed("up") and jump_number > 0:
 			motion.y = -jump_force
 			jump_number -= 1
 			if not is_on_floor() and nextToRightWall():
@@ -49,22 +55,25 @@ func _physics_process(delta):
 			motion.y = 30 * delta
 			if nextToRightWall():
 				sprite.flip_h = true
-				sprite.play("slideWall")
+#				sprite.play("slideWall")
 			elif nextToLeftWall():
 				sprite.flip_h = false
-				sprite.play("slideWall")
+#				sprite.play("slideWall")
 				
 		if !x_input:
 			motion.x = lerp(motion.x, 0, friction)
 	else:
-		if Input.is_action_just_released("ui_up") && motion.y < -jump_force / 2:
+		if Input.is_action_just_released("up") && motion.y < -jump_force / 2:
 			motion.y = -jump_force / 2
 		if !x_input:
 			motion.x = lerp(motion.x, 0, air_friction)
 			
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("dash"):
 		dash()
-		
+	
+	if Input.is_action_just_pressed("attack") && can_attack:
+		attack()
+	
 	motion = move_and_slide(motion, Vector2.UP)
 
 func nextToWall():
@@ -77,9 +86,21 @@ func nextToLeftWall():
 	return $LeftWall.is_colliding()
 	
 func dash():
-	max_speed = 1400
-
+	max_speed = 1700
 	$Timer.start()
+
+func attack():
+	sprite.play("attack")
+	var new_attack = AttackArea.instance()
+	new_attack.dir = direction
+	add_child(new_attack)
+	new_attack.global_position = global_position + direction * 14
+	can_attack = false
+	$AttackTimer.start()
+#	print("attack", direction)
 
 func _on_Timer_timeout():
 	max_speed = 90
+
+func _on_AttackTimer_timeout():
+	can_attack = true
